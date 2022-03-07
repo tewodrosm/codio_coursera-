f7aa0a9255e3680dbf36d78ea3b1263d2ef80094
tewodros mulie <tedmaam2006@gmail.com>
2022-03-07 07:10:35 +0000

    Finish generic reletionships

--- blango/blog/admin.py
+++ blango/blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 # Register your models here.
 admin.site.register(Tag)
+admin.site.register(Comment)
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}

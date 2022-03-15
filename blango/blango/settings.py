2f0175d9d47d2bccf03e3c37226f1bf094cba3f8
tewodros mulie <tedmaam2006@gmail.com>
2022-03-15 09:10:01 +0000

    Finish security ad passwords

--- blango/blango/settings.py
+++ blango/blango/settings.py
@@ -35,6 +35,12 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
 
+PASSWORD_HASHERS = [
+  'django.contrib.auth.hashers.Argon2PasswordHasher',
+  'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+  'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+  'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+]
 
 
 # Application definition

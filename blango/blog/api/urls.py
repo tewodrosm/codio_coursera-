785a0fdf9c2ac132834ae6bd7f652626e17a7dc2
tewodros mulie <tedmaam2006@gmail.com>
2022-04-13 08:36:32 +0000

    Finish viewsets and routers

--- blango/blog/api/urls.py
+++ blango/blog/api/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from blog.api.views import PostList, PostDetail
+from blog.api.views import PostList, PostDetail, TagViewSet, PostViewSet
 
 from rest_framework.authtoken import views
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -20,10 +25,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
-    path("auth/", include("rest_framework.urls")),
-    path("auth/", include("rest_framework.urls")),
+   
     path("token-auth/", views.obtain_auth_token),
 ] 
 
@@ -42,4 +44,5 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
 ]
